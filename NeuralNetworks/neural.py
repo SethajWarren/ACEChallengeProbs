6bed12a31f450d7f83e756cc97d9c5fd32fd534a
SethajWarren <sethajwarren@gmail.com>
2023-06-23 20:08:03 -0500

    began creation of the neural network

--- NeuralNetworks/neural.py
+++ NeuralNetworks/neural.py
@@ -1,10 +1,28 @@
 import re
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.utils import to_categorical
 
 STR_LIST = []
 
+# main function
 def main():
-    pass
 
+    strList = np.zeroes(1,100)
+    print(strList)
+
+    model = Sequential(name="Malware Neural Network")
+    model.add(Flatten(input_shape=(100,1)))
+    model.add(Dense(5, activation='sigmoid'))
+    model.add(Dense(2, activation='softmax'))
+
+    model.summary()
+
+    #model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+
+
+# function to get the strings from a file
 def getStrings(file):
     chars = r"A-Za-z0-9/\-:.,_$%'()[\]<> "
     shortestRun = 4
@@ -13,8 +31,8 @@ def getStrings(file):
 
     return pattern.findall(file)
 
-
-if __name__ == "__name__":
+# start the main function
+if __name__ == "__main__":
     main()
 
 
